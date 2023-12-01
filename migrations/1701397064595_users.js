46de3677a1438e10414f14ed521ebc7f2fde8795
Bryan Maldonado <bmaldonado@sfsu.edu>
2023-11-30 18:29:40 -0800

    user table setup

---
+++ migrations/1701397064595_users.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+/**
+ * @param {import("node-pg-migrate/dist/types").MigrationBuilder} pgm 
+ */
+exports.up = (pgm) => {
+    pgm.createTable("users", {
+        id: "id",
+        email: {
+            type: "varchar(256)",
+            notNull: true,
+            unique: true,
+        },
+        password: {
+            type: "char(60)",
+            notNull: true,
+        },
+        created_at: {
+            type: "timestamp",
+            notNull: true,
+            default: pgm.func("current_timestamp"),
+        },
+    });
+};
+
+
+/**
+ * @param {import("node-pg-migrate/dist/types").MigrationBuilder} pgm 
+ */
+exports.down = (pgm) => {
+    pgm.dropTable("users");
+};
