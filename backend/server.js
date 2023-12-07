4b8cc714613e22be4bb1a6a2bd0cf5fe58a51735
IvaMena <dawn89075@gmail.com>
2023-12-07 00:39:57 -0800

    prettier formatting

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,12 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
 if (process.env.NODE_ENV === "development") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 
 //const testRoutes = require("./routes/test/index.js");
 const requestTime = require("./middleware/request-time");
 const rootRoutes = require("./routes/root");
-
-
 
 const app = express();
 app.use(morgan("dev"));
@@ -26,27 +24,24 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "development") {
-    //require("dotenv").config();
-    const livereload = require("livereload");
-    const connectLiveReload = require("connect-livereload");
-    const liveReloadServer = livereload.createServer();
-    liveReloadServer.watch(path.join(__dirname, "backend", "static"));
-    liveReloadServer.server.once("connection", () => {
-        setTimeout(() => {
-            liveReloadServer.refresh("/");
-        }, 100);
-    });
-    app.use(connectLiveReload());
+  //require("dotenv").config();
+  const livereload = require("livereload");
+  const connectLiveReload = require("connect-livereload");
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+  app.use(connectLiveReload());
 }
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
 
-
-
 //app.use(requestTime); for middleware to check we get response
-
 
 //app.use("/", rootRoutes);
 
@@ -61,10 +56,9 @@ app.use("/lobby", globalLobbyRoutes);
 app.use("/games", gameRoutes);
 
 app.use((_request, _response, next) => {
-    next(createError(404));
+  next(createError(404));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
-
